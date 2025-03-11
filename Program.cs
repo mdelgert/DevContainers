5dc7e7430bad5ac57ae57e31c05e515c9ab46ecf
Matthew Elgert <mdelgert@yahoo.com>
2025-03-10 21:49:18 -0400

    Update loop in Program.cs to iterate 10 times; add DevContainer configuration

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-for (int i = 1; i <= 60; i++)
+for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine($"Hello: {i}");
     Thread.Sleep(1000);
